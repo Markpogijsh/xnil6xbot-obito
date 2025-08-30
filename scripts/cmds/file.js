16125f1e6e976559b950780dc4d411b05499a08c
Markpogijsh <mark123jwj@gmail.com>
2025-08-31 07:09:34 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = [""];
+		const permission = ["61579032975023"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
 		}
